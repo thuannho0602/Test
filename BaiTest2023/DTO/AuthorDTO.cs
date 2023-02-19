ab59c1cb3b22b739d0146af990ab21f8e67a0722
thuannho0602 <thuannho0602@gmail.com>
2023-02-19 15:58:31 +0700

    comit code

--- BaiTest2023/DTO/AuthorDTO.cs
+++ BaiTest2023/DTO/AuthorDTO.cs
@@ -10,7 +10,7 @@
         public DateTime DOB { get; set; }
         public string Address { get; set; }
     }
-    public class AuthorCountBookDTO 
+    public class AuthorCountBookDTO   
     {
         public int Id { get; set; }
         public string Firstname { get; set; }
