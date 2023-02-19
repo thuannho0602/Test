07863f83d90ab71fc455efff47f17fd677373773
thuannho0602 <thuannho0602@gmail.com>
2023-02-19 15:59:34 +0700

    commit

--- BaiTest2023/DTO/AuthorDTO.cs
+++ BaiTest2023/DTO/AuthorDTO.cs
@@ -16,5 +16,6 @@
         public string Firstname { get; set; }
         public string Lastname { get; set; }
         public int TotalBook { get; set; }
+        public string dataaa { get; set; }
     }
 }
